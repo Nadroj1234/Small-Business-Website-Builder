8f6f70ebba3ceb49127efd1da832f0d349fd3fd7
Nadroj1234 <jordank06759@gmail.com>
2026-05-21 20:36:25 -0400

    deploying

--- business-builder/src/firebase/config.js
+++ business-builder/src/firebase/config.js
@@ -1,17 +1,22 @@
-// src/firebase/config.js
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX",
+  apiKey: "AIzaSyBI5JXE9h_uLEcAwOU--ukgizVbOET_S-0",
+  authDomain: "small-business-website-builder.firebaseapp.com",
+  projectId: "small-business-website-builder",
+  storageBucket: "small-business-website-builder.firebasestorage.app",
+  messagingSenderId: "434408802584",
+  appId: "1:434408802584:web:81ea693a45f7615091b66d",
+  measurementId: "G-50R1RZN57Y",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
